866ea9b50a64491c091da0c20feb1f8c657531ca
Ian Hussey <ianhusseysipod@gmail.com>
2024-06-02 19:57:53 +0200

    reorganised causal models lesson

--- causal models/causal_models_collider_looks_like_mediation.R
+++ causal models/causal_models_collider_looks_like_mediation.R
@@ -3,32 +3,35 @@
 # simple demo of collider
 
 analyse <- function(model, data){
+  # fit regressions. NB model must include a Y ~ X term.
   fit <- sem(model = model, data = data) 
   
+  # extract beta estimate for Y~X
   parameterEstimates(fit ) |>
     mutate(effect = paste(lhs, rhs, sep = "~")) |>
     filter(effect == "Y~X") |>
     dplyr::select(beta_estimate = est)
 }
 
-# Y ~ X is zero
+# true Y~X is zero
 dat <- lavaan::simulateData(model = "M ~ 0.5*X + 0.5*Y; Y ~ 0.0*X", sample.nobs = 10000)
-
+# but analysis shows a non-zero Y~X effect!
 analyse(model = "Y ~ X + M", data = dat)
 
 
-# Y ~ X is positive
+# true Y~X is positive
 dat <- lavaan::simulateData(model = "M ~ 0.5*X + 0.5*Y; Y ~ 0.5*X", sample.nobs = 10000)
-
+# but analysis shows a reduced Y~X effect!
 analyse(model = "Y ~ X + M", data = dat)
 
 
-# when positive, it can look just like partial mediation! 
-# a positive direct effect reduces when you control for the mediator
+# when true Y~X is positive, the results can look just like what you would also want to see under a 'partial mediation' model! 
+# ie a positive direct effect reduces when you control for the mediator
 dat <- lavaan::simulateData(model = "M ~ 0.5*X + 0.5*Y; Y ~ 0.5*X", sample.nobs = 10000)
 
+# direct effect "c"
 analyse(model = "Y ~ X", data = dat)
-
+# direct effect "c'" after controlling for mediator
 analyse(model = "Y ~ X + M; M ~ X", data = dat)
 
 # bring up Baron & Kenny's article, how often its cited, and the influence it has on the literature in the last few decades
